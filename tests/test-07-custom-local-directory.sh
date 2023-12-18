44b8ea037a84f877b71c4840fce7651c0d109032
Filip Zyzniewski <filip.zyzniewski@gmail.com>
2023-12-18 23:27:42 +0100

    test: test-07-custom-local-directory: test -l with an existing checkout
    Per
    https://github.com/house-reliability-engineering/gitolize/pull/1/files#r1429114798

--- tests/test-07-custom-local-directory.sh
+++ tests/test-07-custom-local-directory.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
 
-LOCAL_DIRECTORY="$(mktemp -d)"
+LOCAL_DIRECTORY_1="$(mktemp -d)"
+
+git clone \
+  --quiet \
+  "file://$GIT_REPOSITORY" \
+  "$LOCAL_DIRECTORY_1"
 
 gitolize.sh \
-  -l "$LOCAL_DIRECTORY" \
-  -r "file://$GIT_REPOSITORY" \
+  -l "$LOCAL_DIRECTORY_1" \
   -w \
-  bash -c 'echo "test string" > "$GITOLIZE_DIRECTORY/test_file"'
+  bash -c 'echo "test string 1" > "$GITOLIZE_DIRECTORY/test_file_1"'
 
 want_command_output \
-  "test string" \
-  git -C "$GIT_REPOSITORY" show "main:test_file"
+  "test string 1" \
+  git -C "$GIT_REPOSITORY" show "main:test_file_1"
 
 want_file_contents \
-  "test string" \
-  "$LOCAL_DIRECTORY/test_file"
+  "test string 1" \
+  "$LOCAL_DIRECTORY_1/test_file_1"
 
 want_command_output \
-  "test string" \
+  "test string 1" \
   gitolize.sh \
-    -l "$LOCAL_DIRECTORY" \
-    -r "file://$GIT_REPOSITORY" \
-    bash -c 'cat "$GITOLIZE_DIRECTORY/test_file"'
+    -l "$LOCAL_DIRECTORY_1" \
+    bash -c 'cat "$GITOLIZE_DIRECTORY/test_file_1"'
 
-rm -r "$LOCAL_DIRECTORY" "$GIT_REPOSITORY"
+LOCAL_DIRECTORY_2="$(mktemp -d)"
+
+gitolize.sh \
+  -l "$LOCAL_DIRECTORY_2" \
+  -r "file://$GIT_REPOSITORY" \
+  -w \
+  bash -c 'echo "test string 2" > "$GITOLIZE_DIRECTORY/test_file_2"'
+
+want_command_output \
+  "test string 2" \
+  git -C "$GIT_REPOSITORY" show "main:test_file_2"
+
+want_file_contents \
+  "test string 2" \
+  "$LOCAL_DIRECTORY_2/test_file_2"
+
+want_command_output \
+  "test string 2" \
+  gitolize.sh \
+    -l "$LOCAL_DIRECTORY_2" \
+    -r "file://$GIT_REPOSITORY" \
+    bash -c 'cat "$GITOLIZE_DIRECTORY/test_file_2"'
+
+rm -r "$LOCAL_DIRECTORY_1" "$LOCAL_DIRECTORY_2" "$GIT_REPOSITORY"
